db09dfc28551754720813a44e9db0f220ac00bcc
DominikKoltai <dominik.koltai.28@gmail.com>
2023-09-25 08:50:09 +0200

    Springboot initialized

---
+++ src/main/java/com/musicevent/musicevent/MusiceventApplication.java
@@ -0,0 +1,13 @@
+package com.musicevent.musicevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiceventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiceventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicevent/musicevent/MusiceventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicevent.musicevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiceventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
